fdc2e6d9243717e6baa4abb91feb2fff208b4f0e
unknown <minhtam30102002@gmail.com>
2022-09-17 14:13:05 +0700

    Done DFS

--- Week1/DFS/main.py
+++ Week1/DFS/main.py
@@ -51,6 +51,22 @@ if __name__ == "__main__":
     # graph.addEdge(4, 4)
     # graph.addEdge(5, 2)
 
+    graph.addEdge(0, 1)
+    graph.addEdge(0, 2)
+    graph.addEdge(0, 4)
+    graph.addEdge(0, 7)
+    graph.addEdge(1, 2)
+    graph.addEdge(1, 5)
+    graph.addEdge(1, 6)
+    graph.addEdge(2, 0)
+    graph.addEdge(2, 3)
+    graph.addEdge(3, 3)
+    graph.addEdge(4, 4)
+    graph.addEdge(4, 7)
+    graph.addEdge(5, 2)
+    graph.addEdge(6, 5)
+    graph.addEdge(7, 7)
+
     
     graph.printEdge()
     graph.searchDFS(2)
